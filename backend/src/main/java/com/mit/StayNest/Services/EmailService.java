c6cb3f7e6a6f32f5e0e8fd9b8fafdff5ff64e027
Sahas2711 <sahasnagar1234@gmail.com>
2026-01-05 16:40:26 +0530

    Added Health Check point in backend

--- backend/src/main/java/com/mit/StayNest/Services/EmailService.java
+++ backend/src/main/java/com/mit/StayNest/Services/EmailService.java
@@ -1,11 +1,7 @@
 package com.mit.StayNest.Services;
 
-import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.mit.StayNest.Entity.Booking;
